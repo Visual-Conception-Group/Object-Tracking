ae985644c2720918a40fc07b3b6e6c307773ef01
Monika2507 <33967943+Monika2507@users.noreply.github.com>
2022-03-15 17:32:50 +0530

    Delete 2_CGRCF directory

--- 2_CGRCF/ADMM_solve_q.m
+++
@@ -1,9 +0,0 @@
-function channel_weight=ADMM_solve_q(mu,beta,T,h_f,l_f,g_f,channel_weight,L)
-     h_f=h_f(:,:,1:31);
-     g_f=g_f(:,:,1:31);
-     l_f=l_f(:,:,1:31);
-     lhd= T ./  (h_f .^2 *mu*T - beta); 
-     X=ifft2(mu*g_f + l_f);
-     channel_weight=bsxfun(@times,lhd,X);
-
-end
